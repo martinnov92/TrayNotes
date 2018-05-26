29dc51a005d92bd0be7608c293b986a85ad940b2
Martin Novák <martinnovak@outlook.com>
2018-05-26 18:59:22 +0200

    app, form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,77 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            value: '',
+            events: [],
+        };
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        });
+    }
+
+    handleNoteSave = (e) => {
+        e.preventDefault();
+
+        if (this.state.value.length === 0) {
+            return;
+        }
+
+        const events = [...this.state.events];
+        events.push(this.state.value);
+
+        this.setState({
+            events,
+            value: '',
+        });
+    }
+
     render() {
+        const {
+            value,
+            events,
+        } = this.state;
+
         return (
             <React.Fragment>
                 <div className="triangle" />
+
                 <div
                     className="app"
                 >
-                    aplikace
+                    <form
+                        className="input-with-button"
+                        onSubmit={this.handleNoteSave}
+                    >  
+                        <input
+                            type="text"
+                            value={value}
+                            onChange={this.handleInputChange}
+                        />
+                        <button
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                    </form>
+
+                    <ul>
+                        {
+                            events.map((event) => {
+                                return (
+                                    <li>
+                                        { event }
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
                 </div>
             </React.Fragment>
         );
